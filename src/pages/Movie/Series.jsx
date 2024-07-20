ee4e52677ac7708f9c4c7882a0e3088d7ffe7948
Ludis <leulsegedmelaku1020@gmail.com>
2024-07-20 20:32:03 +0300

    "Added react-hot-toast dependency and implemented add/remove from About To feature in Series component"

--- src/pages/Movie/Series.jsx
+++ src/pages/Movie/Series.jsx
@@ -5,6 +5,17 @@ import { Button } from "../../components";
 import { format } from "date-fns";
 import { Movies } from "../Home/Movies";
 import { Footer } from "../../components";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
+import { useAuth } from "../../context/AuthContext";
+import toast, { Toaster } from "react-hot-toast";
 
 export const Series = () => {
   const { id } = useParams();
@@ -12,7 +23,44 @@ export const Series = () => {
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(true);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [buttonLoading, setButtonLoading] = useState(false);
+  const [isInAboutTo, setIsInAboutTo] = useState(false);
+  const { db, currentUser } = useAuth();
 
+  const addOrRemoveAboutTo = async () => {
+    setButtonLoading(true);
+    const currentTime = new Date();
+    const aboutToData = {
+      movieId: Number(id),
+      time: currentTime.toISOString(),
+      userId: currentUser.uid,
+      type: "tv",
+    };
+
+    try {
+      if (isInAboutTo) {
+        const q = query(
+          collection(db, "about-to"),
+          where("movieId", "==", Number(id)),
+          where("userId", "==", currentUser.uid)
+        );
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach(async (doc) => {
+          await deleteDoc(doc.ref);
+        });
+        toast.success("Removed from About To successfully.");
+      } else {
+        await addDoc(collection(db, "about-to"), aboutToData);
+        toast.success("Added to About To successfully.");
+      }
+      setIsInAboutTo(!isInAboutTo);
+    } catch (error) {
+      toast.error("Failed to update About To.");
+      console.error("Error updating About To:", error);
+    } finally {
+      setButtonLoading(false);
+    }
+  };
   useEffect(() => {
     setLoading(true);
     const fetchMovie = async () => {
@@ -28,6 +76,7 @@ export const Series = () => {
         setImages(img.posters);
       } catch (error) {
         setLoading(false);
+        toast.error("Error fetching movie data.");
         console.error("Error fetching movie:", error);
       } finally {
         setLoading(false);
@@ -67,6 +116,7 @@ export const Series = () => {
   }
   return (
     <main className="profile-page">
+      <Toaster />
       <section className="relative block h-500-px">
         <div
           className="absolute top-0 w-full h-full bg-center bg-cover"
@@ -165,13 +215,38 @@ export const Series = () => {
                 <div className="mb-2 flex flex-wrap gap-4 justify-center text-white mt-10">
                   {movie.genres.map((g) => (
                     <button class="bg-red-950 text-red-400 border border-red-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
-                      <span class="bg-red-400 shadow-red-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
-                      {g.name}
+                      <span class="bg-red-400 shadow-red-400 absolute -top-[150%] left-0 inline-flex w-80 h-80 rounded-full opacity-30 transition-all duration-500 group-hover:top-[25%]"></span>
+                      <span class="flex items-center gap-2 text-inherit transition group-hover:scale-90">
+                        <span class="relative -ml-2 inline-block h-3 w-3 rotate-45 rounded-sm bg-gradient-to-tr from-red-200 to-red-500 shadow-[0_0_4px_#0005,0_1px_2px_#0003,0_2px_4px_#0002] transition duration-500 group-hover:scale-125 group-hover:rotate-[225deg]"></span>
+                        <span class="transition-colors duration-200 group-hover:text-white">
+                          {g.name}
+                        </span>
+                      </span>
                     </button>
                   ))}
                 </div>
                 <div className="mb-2 text-white">
                   {movie.vote_count} people voted it
+                </div>
+                <div className="mb-2 ">
+                  <button
+                    onClick={addOrRemoveAboutTo}
+                    className={`group group-hover:before:duration-500 pr-24 group-hover:after:duration-1000 after:duration-500 hover:border-red-300  duration-500 before:duration-500 hover:duration-500 underline underline-offset-2 hover:after:-right-2 hover:before:top-8 hover:before:right-16 hover:after:scale-150 hover:after:blur-none hover:before:-bottom-8 hover:before:blur-none hover:bg-red-300 hover:underline hover:underline-offset-4  origin-left hover:decoration-2 hover:text-red-900 relative bg-red-800 h-16 w-64 border text-left p-3 text-gray-50 text-base font-bold rounded-lg  overflow-hidden before:absolute before:w-12 before:h-12 before:content[''] before:right-1 before:top-1 before:z-10 before:bg-red-400 before:rounded-full before:blur-lg  after:absolute after:z-10 after:w-20 after:h-20 after:content['']  after:bg-[#333] after:right-8 after:top-3 after:rounded-full after:blur ${
+                      buttonLoading ? "opacity-50 cursor-not-allowed" : ""
+                    }`}
+                    disabled={buttonLoading}
+                  >
+                    {buttonLoading ? (
+                      <div
+                        class="loader border-t-2 rounded-full border-gray-500 bg-white animate-spin
+aspect-square w-8 flex justify-center items-center text-yellow-700"
+                      ></div>
+                    ) : isInAboutTo ? (
+                      "Remove from About To"
+                    ) : (
+                      "Add to About To"
+                    )}
+                  </button>
                 </div>
               </div>
               <div className="mt-10 py-10 border-t border-white text-center">
@@ -195,7 +270,7 @@ export const Series = () => {
                   </thead>
                   <tbody>
                     {movie.seasons.slice(1).map((m) => (
-                      <tr>
+                      <tr key={m.id}>
                         <td>
                           <div className="flex items-center gap-3">
                             <div className="avatar">
