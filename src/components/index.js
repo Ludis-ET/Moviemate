4aeeebc61b8c0427d5591004e2c5ca051365106a
Ludis <leulsegedmelaku1020@gmail.com>
2024-10-11 19:44:12 +0300

    Remove unnecessary files and code

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { SideBarLeft } from "./Left/SideBar";
-export { SideBarRight } from "./Right/SideBar";
-export { LikeButton } from "./Random/LikeButton";
-export { Button } from "./Random/Button";
-export { DeleteButton } from "./Random/DeleteButton";
-export { Footer } from "./Footer";
-export { Tool } from "./Random/Tool";
--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-export { Middle } from "./Home/Middle";
-export { Results } from "./Search/Results";
-export { List } from "./About to/List";
-export { Movie } from "./Movie/Movie";
-export { MovieList } from "./List/MovieList";
-export { TvList } from "./List/TvList";
-export { Leaderboard } from "./Leaderboard/Leaderboard";
-export { Series } from "./Movie/Series";
