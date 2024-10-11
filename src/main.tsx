6e33ec92918e9c2088d9de2b1ffabdaa0f79b819
Ludis <leulsegedmelaku1020@gmail.com>
2024-10-11 21:58:13 +0300

    Refactor project configuration files and update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+import { BrowserRouter as Router } from "react-router-dom";
+
+import App from "./App.tsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <App />
+    </Router>
+  </StrictMode>
+);
